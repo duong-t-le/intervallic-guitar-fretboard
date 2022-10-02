536309e5f1ea483c6c8ffd7683f04ce204fc26c8
D <duong.thai.le@gmail.com>
2022-10-02 14:06:36 -0500

    Updates

---
+++ precache-manifest.89f111608e73cf1308b49fb6115ea40a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff827d0c08f8f1fd330b",
+    "url": "/intervallic-guitar-fretboard/static/css/main.a453ef33.chunk.css"
+  },
+  {
+    "revision": "ff827d0c08f8f1fd330b",
+    "url": "/intervallic-guitar-fretboard/static/js/main.6f27fb27.chunk.js"
+  },
+  {
+    "revision": "43336a77cf954da3892d",
+    "url": "/intervallic-guitar-fretboard/static/js/runtime~main.43b491ed.js"
+  },
+  {
+    "revision": "c4e34a00945934fe9945",
+    "url": "/intervallic-guitar-fretboard/static/js/2.9d25c33c.chunk.js"
+  },
+  {
+    "revision": "79be80b375553eb66abb",
+    "url": "/intervallic-guitar-fretboard/static/js/3.c70b6b5b.chunk.js"
+  },
+  {
+    "revision": "c72a35ebd8717a7c2880b9f3b93ab3cd",
+    "url": "/intervallic-guitar-fretboard/index.html"
+  }
+];
